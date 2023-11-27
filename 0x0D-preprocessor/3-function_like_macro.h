0aeead1c6e0805cab9f4ed1123973dfb7f18d51f
Mouad55ya <mouadyaich5@gmail.com>
2023-11-27 23:35:48 +0100

    creating

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef function_like_macro
-#ifdef function_like_macro
+#ifndef function_like_macro_h
+#define function_like_macro_h
 #ifndef ABS
 #define ABS(x) ((x) >= (0) ? (x) : ((x) * (-1)))
 #endif
